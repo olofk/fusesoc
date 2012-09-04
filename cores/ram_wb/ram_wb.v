34768c38e90f8d338a7259b67aadc1ffd9df37b4
Olof Kindgren <olof@opencores.org>
2012-09-04 21:19:25 +0200

    Converted ram_wb.v to verilog 2k1

--- cores/ram_wb/ram_wb.v
+++ cores/ram_wb/ram_wb.v
@@ -1,25 +1,50 @@
 module ram_wb
   (
-   // Inputs
-   wbm0_adr_i, wbm0_bte_i, wbm0_cti_i, wbm0_cyc_i, wbm0_dat_i, wbm0_sel_i, 
-   wbm0_stb_i, wbm0_we_i,
-   // Outputs
-   wbm0_ack_o, wbm0_err_o, wbm0_rty_o, wbm0_dat_o,
-  
-   // Inputs
-   wbm1_adr_i, wbm1_bte_i, wbm1_cti_i, wbm1_cyc_i, wbm1_dat_i, wbm1_sel_i, 
-   wbm1_stb_i, wbm1_we_i,
-   // Outputs
-   wbm1_ack_o, wbm1_err_o, wbm1_rty_o, wbm1_dat_o,
-
-    // Inputs
-   wbm2_adr_i, wbm2_bte_i, wbm2_cti_i, wbm2_cyc_i, wbm2_dat_i, wbm2_sel_i, 
-   wbm2_stb_i, wbm2_we_i,
-   // Outputs
-   wbm2_ack_o, wbm2_err_o, wbm2_rty_o, wbm2_dat_o,
-
    // Clock, reset
-   wb_clk_i, wb_rst_i
+   input 		wb_clk_i,
+   input 		wb_rst_i,
+   // Inputs
+   input [aw-1:0]	wbm0_adr_i,
+   input [1:0] 		wbm0_bte_i,
+   input [2:0] 		wbm0_cti_i,
+   input 		wbm0_cyc_i,
+   input [dw-1:0] 	wbm0_dat_i,
+   input [3:0] 		wbm0_sel_i,
+   input 		wbm0_stb_i,
+   input 		wbm0_we_i,
+   // Outputs
+   output 		wbm0_ack_o,
+   output 		wbm0_err_o,
+   output 		wbm0_rty_o,
+   output [dw-1:0] 	wbm0_dat_o,
+   // Inputs
+   input [aw-1:0]	wbm1_adr_i,
+   input [1:0] 		wbm1_bte_i,
+   input [2:0] 		wbm1_cti_i,
+   input 		wbm1_cyc_i,
+   input [dw-1:0] 	wbm1_dat_i,
+   input [3:0] 		wbm1_sel_i,
+   input 		wbm1_stb_i,
+   input 		wbm1_we_i,
+   // Outputs
+   output 		wbm1_ack_o,
+   output 		wbm1_err_o,
+   output 		wbm1_rty_o,
+   output [dw-1:0] 	wbm1_dat_o,
+    // Inputs
+   input [aw-1:0]	wbm2_adr_i,
+   input [1:0] 		wbm2_bte_i,
+   input [2:0] 		wbm2_cti_i,
+   input 		wbm2_cyc_i,
+   input [dw-1:0] 	wbm2_dat_i,
+   input [3:0] 		wbm2_sel_i,
+   input 		wbm2_stb_i,
+   input 		wbm2_we_i,
+   // Outputs
+   output 		wbm2_ack_o,
+   output 		wbm2_err_o,
+   output 		wbm2_rty_o,
+   output [dw-1:0] 	wbm2_dat_o
    );
    // Bus parameters
    parameter dw = 32;
@@ -29,75 +54,21 @@ module ram_wb
    parameter mem_adr_width = 10; //(log2(mem_span));
    parameter memory_file = "";
 
-   input [aw-1:0]	wbm0_adr_i;
-   input [1:0] 		wbm0_bte_i;
-   input [2:0] 		wbm0_cti_i;
-   input 		wbm0_cyc_i;
-   input [dw-1:0] 	wbm0_dat_i;
-   input [3:0] 		wbm0_sel_i;
-   input 		wbm0_stb_i;
-   input 		wbm0_we_i;
-   
-   output 		wbm0_ack_o;
-   output 		wbm0_err_o;
-   output 		wbm0_rty_o;
-   output [dw-1:0] 	wbm0_dat_o;
-
-   input [aw-1:0]	wbm1_adr_i;
-   input [1:0] 		wbm1_bte_i;
-   input [2:0] 		wbm1_cti_i;
-   input 		wbm1_cyc_i;
-   input [dw-1:0] 	wbm1_dat_i;
-   input [3:0] 		wbm1_sel_i;
-   input 		wbm1_stb_i;
-   input 		wbm1_we_i;
-   
-   output 		wbm1_ack_o;
-   output 		wbm1_err_o;
-   output 		wbm1_rty_o;
-   output [dw-1:0] 	wbm1_dat_o;
-
-   input [aw-1:0]	wbm2_adr_i;
-   input [1:0] 		wbm2_bte_i;
-   input [2:0] 		wbm2_cti_i;
-   input 		wbm2_cyc_i;
-   input [dw-1:0] 	wbm2_dat_i;
-   input [3:0] 		wbm2_sel_i;
-   input 		wbm2_stb_i;
-   input 		wbm2_we_i;
-   
-   output 		wbm2_ack_o;
-   output 		wbm2_err_o;
-   output 		wbm2_rty_o;
-   output [dw-1:0] 	wbm2_dat_o;
-   
-
-   input 		wb_clk_i;
-   input 		wb_rst_i;
-
    // Internal wires to actual RAM
-   wire [aw-1:0] 	wbs_ram_adr_i;
-   wire [1:0] 		wbs_ram_bte_i;
-   wire [2:0] 		wbs_ram_cti_i;
-   wire 		wbs_ram_cyc_i;
-   wire [dw-1:0] 	wbs_ram_dat_i;
-   wire [3:0] 		wbs_ram_sel_i;
-   wire 		wbs_ram_stb_i;
-   wire 		wbs_ram_we_i;
-   
+
    wire 		wbs_ram_ack_o;
    wire 		wbs_ram_err_o;
    wire 		wbs_ram_rty_o;
    wire [dw-1:0] 	wbs_ram_dat_o;
 
    reg [2:0] 		input_select, last_selected;
-   wire 		arb_for_wbm0, arb_for_wbm1, arb_for_wbm2;
+
    // Wires allowing selection of new input
-   assign arb_for_wbm0 = (last_selected[1] | last_selected[2] | 
+   wire arb_for_wbm0 = (last_selected[1] | last_selected[2] | 
 			  !wbm1_cyc_i | !wbm2_cyc_i) & !(|input_select);
-   assign arb_for_wbm1 = (last_selected[0] | last_selected[2] | 
+   wire arb_for_wbm1 = (last_selected[0] | last_selected[2] | 
 			  !wbm0_cyc_i | !wbm2_cyc_i) & !(|input_select);
-   assign arb_for_wbm2 = (last_selected[0] | last_selected[1] | 
+   wire arb_for_wbm2 = (last_selected[0] | last_selected[1] | 
 			  !wbm0_cyc_i | !wbm1_cyc_i) & !(|input_select);
    
    // Master select logic
@@ -124,31 +95,31 @@ module ram_wb
      else if (!(&input_select) & wbm2_cyc_i & arb_for_wbm2)
        last_selected <= 3'b100;
 
-      // Mux input signals to RAM (default to wbm0)
-   assign wbs_ram_adr_i = (input_select[2]) ? wbm2_adr_i : 
-			    (input_select[1]) ? wbm1_adr_i : 
-			    (input_select[0]) ? wbm0_adr_i : 0;
-   assign wbs_ram_bte_i = (input_select[2]) ? wbm2_bte_i : 
-			    (input_select[1]) ? wbm1_bte_i : 
-			    (input_select[0]) ? wbm0_bte_i : 0;
-   assign wbs_ram_cti_i = (input_select[2]) ? wbm2_cti_i : 
-			    (input_select[1]) ? wbm1_cti_i : 
-			    (input_select[0]) ? wbm0_cti_i : 0;
-   assign wbs_ram_cyc_i = (input_select[2]) ? wbm2_cyc_i : 
-			    (input_select[1]) ? wbm1_cyc_i : 
-			    (input_select[0]) ? wbm0_cyc_i : 0;
-   assign wbs_ram_dat_i = (input_select[2]) ? wbm2_dat_i : 
-			    (input_select[1]) ? wbm1_dat_i : 
-			    (input_select[0]) ? wbm0_dat_i : 0;
-   assign wbs_ram_sel_i = (input_select[2]) ? wbm2_sel_i : 
-			    (input_select[1]) ? wbm1_sel_i : 
-			    (input_select[0]) ? wbm0_sel_i : 0;
-   assign wbs_ram_stb_i = (input_select[2]) ? wbm2_stb_i : 
-			    (input_select[1]) ? wbm1_stb_i : 
-			    (input_select[0]) ? wbm0_stb_i : 0;
-   assign wbs_ram_we_i  = (input_select[2]) ? wbm2_we_i  :
-			    (input_select[1]) ? wbm1_we_i  : 
-			    (input_select[0]) ? wbm0_we_i : 0;
+   // Mux input signals to RAM (default to wbm0)
+   wire [aw-1:0] wbs_ram_adr_i = (input_select[2]) ? wbm2_adr_i : 
+		 (input_select[1]) ? wbm1_adr_i : 
+		 (input_select[0]) ? wbm0_adr_i : 0;
+   wire [1:0] 	 wbs_ram_bte_i = (input_select[2]) ? wbm2_bte_i : 
+		 (input_select[1]) ? wbm1_bte_i : 
+		 (input_select[0]) ? wbm0_bte_i : 0;
+   wire [2:0] 	 wbs_ram_cti_i = (input_select[2]) ? wbm2_cti_i : 
+		 (input_select[1]) ? wbm1_cti_i : 
+		 (input_select[0]) ? wbm0_cti_i : 0;
+   wire 	 wbs_ram_cyc_i = (input_select[2]) ? wbm2_cyc_i : 
+		 (input_select[1]) ? wbm1_cyc_i : 
+		 (input_select[0]) ? wbm0_cyc_i : 0;
+   wire [dw-1:0] wbs_ram_dat_i = (input_select[2]) ? wbm2_dat_i : 
+		 (input_select[1]) ? wbm1_dat_i : 
+		 (input_select[0]) ? wbm0_dat_i : 0;
+   wire [3:0] 	 wbs_ram_sel_i = (input_select[2]) ? wbm2_sel_i : 
+		 (input_select[1]) ? wbm1_sel_i : 
+		 (input_select[0]) ? wbm0_sel_i : 0;
+   wire 	 wbs_ram_stb_i = (input_select[2]) ? wbm2_stb_i : 
+		 (input_select[1]) ? wbm1_stb_i : 
+		 (input_select[0]) ? wbm0_stb_i : 0;
+   wire 	 wbs_ram_we_i  = (input_select[2]) ? wbm2_we_i  :
+		 (input_select[1]) ? wbm1_we_i  : 
+		 (input_select[0]) ? wbm0_we_i : 0;
 
    // Output from RAM, gate the ACK, ERR, RTY signals appropriately
    assign wbm0_dat_o = wbs_ram_dat_o;
@@ -166,7 +137,13 @@ module ram_wb
    assign wbm2_err_o = wbs_ram_err_o & input_select[2];
    assign wbm2_rty_o = 0;
    
-   ram_wb_b3 #(.memory_file(memory_file)) ram_wb_b3_0
+   ram_wb_b3
+     #(.memory_file(memory_file),
+       .aw(aw),
+       .dw(dw),
+       .mem_size_bytes(mem_size_bytes),
+       .mem_adr_width(mem_adr_width))
+   ram_wb_b3_0
      (
       // Outputs
       .wb_ack_o				(wbs_ram_ack_o),
@@ -184,11 +161,6 @@ module ram_wb
       .wb_we_i				(wbs_ram_we_i),
       .wb_clk_i				(wb_clk_i),
       .wb_rst_i				(wb_rst_i));
-
-   defparam ram_wb_b3_0.aw = aw;
-   defparam ram_wb_b3_0.dw = dw;
-   defparam ram_wb_b3_0.mem_size_bytes = mem_size_bytes;
-   defparam ram_wb_b3_0.mem_adr_width = mem_adr_width;
 
 endmodule // ram_wb
 
