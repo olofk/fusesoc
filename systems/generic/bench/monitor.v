3e52f79b9312657399824820698e9a1282ce2661
Olof Kindgren <olof@opencores.org>
2012-10-21 00:36:33 +0200

    Inform that testcase was stopped by nop

--- systems/generic/bench/monitor.v
+++ systems/generic/bench/monitor.v
@@ -7,6 +7,7 @@ module or1200_monitor
    always @(posedge clk) begin
       if (wb_insn == 32'h1500_0001) begin
 	 r3 = orpsoc_tb.dut.or1200_top0.or1200_cpu.or1200_rf.rf_a.get_gpr(5'b00011);
+	 $display("nop 0x1 received after %d",$time);
 	 $display("exit(%0d)",r3);
 	 $finish;
       end
