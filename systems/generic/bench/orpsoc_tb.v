a6ee855fa16066be3a06b1939bb756b47a443008
Olof Kindgren <olof@opencores.org>
2012-12-24 21:35:59 +0100

    generic.system: Replace ram_wb_loader with or1k elf loader

--- systems/generic/bench/orpsoc_tb.v
+++ systems/generic/bench/orpsoc_tb.v
@@ -14,8 +14,23 @@ module orpsoc_tb;
    end
 
    vlog_tb_utils vlog_tb_utils0();
-   ram_wb_loader ram_wb_loader0();
 
+   integer mem_words;
+   integer i;
+   reg [31:0] mem_word;
+   reg [1023:0] elf_file;
+
+   initial begin
+      if($value$plusargs("or1k_elf_load=%s", elf_file)) begin
+	 $or1k_elf_load_file(elf_file);
+      
+	 mem_words = $or1k_elf_get_size/4;
+	 for(i=0; i < mem_words; i = i+1)
+	   orpsoc_tb.dut.ram_wb0.ram_wb_b3_0.mem[i] = $or1k_elf_read_32(i*4);
+      end else
+	$display("No ELF file specified");
+   end
+   
    reg enable_jtag_vpi;
    initial enable_jtag_vpi = $test$plusargs("enable_jtag_vpi");
    
