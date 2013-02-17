4b1b1092964222a7bfc1b66845dfb9d478fe162e
Per Larsson <pela@opencores.org>
2013-02-17 01:44:39 +0100

    Added logging to opencores provider

--- orpsoc/provider/opencores.py
+++ orpsoc/provider/opencores.py
@@ -2,16 +2,21 @@ from orpsoc.provider import Provider
 
 import subprocess
 import os.path
+import logging
+
+logger = logging.getLogger(__name__)
 
 class ProviderOpenCores(Provider):
     def __init__(self, config):
+        logger.debug('__init__() *Entered*')
         self.repo_path = 'http://opencores.org/ocsvn/' + \
             config.get('repo_name') + '/' + config.get('repo_name') + '/' + \
             config.get('repo_root')
         self.revision_number  = config.get('revision')
+        logger.debug('__init__() -Done-')
 
     def fetch(self, local_dir):
-
+        logger.debug('fetch() *Entered*')
         status = self.status(local_dir)
 
         if status == 'empty':
@@ -25,8 +30,10 @@ class ProviderOpenCores(Provider):
             pass
         else:
             print("provider status is: " + status + " This shouldn't happen")
+        logger.debug('fetch() -Done-')
 
     def status(self, local_dir):
+        logger.debug('status() *Entered*')
         #FIXME: Check if repo is modified, or is an SVN repo at all, etc..
         if not os.path.isdir(local_dir):
             return 'empty'
@@ -34,6 +41,7 @@ class ProviderOpenCores(Provider):
             return 'downloaded'
         
     def _checkout(self, local_dir):
+        logger.debug('_checkout() *Entered*')
         print("Checking out " + self.repo_path + " revision " + self.revision_number + " to " + local_dir)
         try:
             subprocess.check_call(['svn', 'co', '-q', '--no-auth-cache',
@@ -48,6 +56,7 @@ class ProviderOpenCores(Provider):
         except subprocess.CalledProcessError:
             print("Error: Failed to checkout " + self.repo_path)
             exit(1)
+        logger.debug('_checkout() -Done-')
 
     def _update(self):
         pass
