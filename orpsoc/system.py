41f04cdcab98ca9da849d5f67aa16625815d2a64
Per Larsson <pela@opencores.org>
2013-02-17 02:05:13 +0100

    Added logging to system class

--- orpsoc/system.py
+++ orpsoc/system.py
@@ -8,9 +8,15 @@ from orpsoc.core import Core
 from orpsoc.config import Config
 from orpsoc.verilog import Verilog
 import os
+import logging
+
+logger = logging.getLogger(__name__)
 
 class System:
     def __init__(self, system_file):
+        logger.debug('__init__() *Entered*' +
+                     '\n    system_file=' + str(system_file)
+                    )
         system_root = os.path.dirname(system_file)
 
         self.config = configparser.SafeConfigParser()
@@ -35,11 +41,17 @@ class System:
             if self.backend_name and self.config.has_section(self.backend_name):
                 self.backend = dict(self.config.items(self.backend_name))
 
+        logger.debug('self.cores=' + str(self.cores))
+        logger.debug('__init__() -Done-')
+
     def setup_cores(self):
+        logger.debug('setup_cores() *Entered*')
         for core in self.cores:
             self.cores[core].setup()
+        logger.debug('setup_cores() -Done-')
 
     def _create_orpsoc_core(self, config, system_root):
+        logger.debug('_create_orpsoc_core() *Entered*')
         core = Core(name=self.name, core_root=system_root)
         if config.has_section('plusargs'):
             core.vpi = Plusargs(dict(config.items('plusargs')))
@@ -52,10 +64,13 @@ class System:
         return core
         
     def get_cores(self):
+        logger.debug('get_cores() *Entered*')
         return self.cores
 
     def get_src_files(self):
+        logger.debug('get_src_files() *Entered*')
         return self.src_files
             #FIXME: Iterate through core RTL files and append to rtl_files
     def get_tb_files(self):
+        logger.debug('get_tb_files() *Entered*')
         return self.tb_files
