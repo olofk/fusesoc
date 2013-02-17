a2fe08c941da6dd8d18868febdbb268ae3da3b40
Per Larsson <pela@opencores.org>
2013-02-17 02:00:43 +0100

    Added logging to core class

--- orpsoc/core.py
+++ orpsoc/core.py
@@ -12,9 +12,17 @@ from orpsoc.verilog import Verilog
 import os
 import shutil
 import subprocess
+import logging
+
+logger = logging.getLogger(__name__)
 
 class Core:
     def __init__(self, core_file=None, name=None, core_root=None):
+        logger.debug('__init__() *Entered*' +
+                     '\n    core_file=' + str(core_file) +
+                     '\n    name=' + str(name) + 
+                     '\n    core_root=' + str(core_root)
+                    )
         if core_file:
             basename = os.path.basename(core_file)
         self.plusargs = None
@@ -44,6 +52,7 @@ class Core:
             else:
                 self.name = basename.split('.core')[0]
 
+            logger.debug('name=' + str(self.name))
             self.core_root = os.path.dirname(core_file)
 
             if config.has_section('plusargs'):
@@ -59,7 +68,9 @@ class Core:
             if config.has_section('verilog'):
                 items = config.items('verilog')
                 self.verilog = Verilog(dict(items))
-
+                logger.debug('verilog.src_files=' + str(self.verilog.src_files))
+                logger.debug('verilog.include_files=' + str(self.verilog.include_files))
+                logger.debug('verilog.include_dirs=' + str(self.verilog.include_dirs))
             if config.has_section('vpi'):
                 items = config.items('vpi')
                 self.vpi = VPI(dict(items))
@@ -72,18 +83,26 @@ class Core:
             self.files_root = core_root
 
             self.provider = None
+        logger.debug('__init__() -Done-')
+
 
     def cache_status(self):
+        logger.debug('cache_status() *Entered*')
         if self.provider:
             return self.provider.status(self.cache_dir)
         else:
             return 'local'
 
     def setup(self):
+        logger.debug('setup() *Entered*')
+        logger.debug("  name="+self.name)
         if self.provider:
             self.provider.fetch(self.cache_dir)
+        logger.debug('setup() -Done-')
 
     def export(self, dst_dir):
+        logger.debug('export() *Entered*')
+        logger.debug("  name="+self.name)
         if os.path.exists(dst_dir):
             shutil.rmtree(dst_dir)
 
@@ -97,6 +116,8 @@ class Core:
             src_files += self.vpi.export()
 
         dirs = list(set(map(os.path.dirname,src_files)))
+        logger.debug("export src_files=" + str(src_files))
+        logger.debug("export dirs=" + str(dirs))
         for d in dirs:
             if not os.path.exists(os.path.join(dst_dir, d)):
                 os.makedirs(os.path.join(dst_dir, d))
@@ -106,15 +127,21 @@ class Core:
                 shutil.copyfile(os.path.join(src_dir, f), 
                                 os.path.join(dst_dir, f))
             else:
+                logger.debug("  File " + f + " doesn't exist - may it will be added as patch later?")
                 print("File " + os.path.join(src_dir, f) + " doesn't exist")
                 exit(1)
+        logger.debug('export() -Done-')
         
     def patch(self, dst_dir):
+        logger.debug('patch() *Entered*')
+        logger.debug("  name=" + self.name)
         #FIXME: Use native python patch instead
         patch_root = os.path.join(Config().cores_root, self.name, 'patches')
         if os.path.exists(patch_root):
             for f in sorted(os.listdir(patch_root)):
                 if os.path.isfile(os.path.join(patch_root, f)):
+                    logger.debug("  applying patch file: " + os.path.join(patch_root, f) + "\n" +
+                                 "                   to: " + os.path.join(dst_dir))
                     try:
                         subprocess.call(['patch','-p1', '-s',
                                          '-d', os.path.join(dst_dir),
@@ -122,9 +149,11 @@ class Core:
                     except OSError:
                         print("Error: Failed to call external command 'patch'")
                         return False
+        logger.debug('patch() -Done-')
         return True
 
     def info(self):
+        logger.debug('info() *Entered*')
         # TODO: finish and make look better
         print "CORE INFO"
         print "Name                  :", self.name
@@ -132,3 +161,6 @@ class Core:
         #if core_file:
         #    print 
         #    if config.has_section('provider'):
+        logger.debug('info() -Done-')
+
+
