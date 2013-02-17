d33ebb1becb0398f2cc33b32a0ebadb23da6e82b
Per Larsson <pela@opencores.org>
2013-02-17 01:49:35 +0100

    Added logging to simulator base class

--- orpsoc/simulator/simulator.py
+++ orpsoc/simulator/simulator.py
@@ -2,10 +2,14 @@ from orpsoc.config import Config
 import argparse
 import shutil
 import os
+import logging
+
+logger = logging.getLogger(__name__)
 
 class Simulator(object):
 
     def __init__(self, system):
+        logger.debug('__init__() *Entered*')
         config = Config()
         self.system = system
         self.build_root = os.path.join(config.build_root, self.system.name)
@@ -18,14 +22,21 @@ class Simulator(object):
         self.src_files = []
 
         for core_name in self.system.get_cores():
+            logger.debug('core_name=' + core_name)
             core = self.system.cores[core_name]
             if core.verilog:
+                if core.verilog.include_dirs:
+                    logger.debug('core.include_dirs=' + str(core.verilog.include_dirs))
+                else:
+                    logger.debug('core.include_dirs=None')
                 self.include_dirs += [os.path.join(self.src_root, core_name, d) for d in core.verilog.include_dirs]
                 self.include_dirs += [os.path.join(self.src_root, core_name, d) for d in core.verilog.tb_include_dirs]
                 self.src_files    += [os.path.join(self.src_root, core_name, f) for f in core.verilog.src_files]
                 self.src_files    += [os.path.join(self.src_root, core_name, f) for f in core.verilog.tb_src_files]
+        logger.debug('__init__() -Done-')
 
     def configure(self):
+        logger.debug('configure() *Entered*')
         if os.path.exists(self.sim_root):
             shutil.rmtree(self.sim_root)
         os.makedirs(self.sim_root)
@@ -38,8 +49,10 @@ class Simulator(object):
             core.setup()
             core.export(dst_dir)
             core.patch(dst_dir)
+        logger.debug('configure() -Done-')
 
     def build(self):
+        logger.debug('build() *Entered*')
         self.vpi_modules = []
         for name, core in self.system.get_cores().items():
             if core.vpi:
@@ -49,9 +62,11 @@ class Simulator(object):
                 vpi_module['src_files']     = [os.path.join(core_root, f) for f in core.vpi.src_files]
                 vpi_module['name']          = core.vpi.name
                 self.vpi_modules += [vpi_module]
+        logger.debug('build() -Done-')
 
     def run(self, args):
-        
+        logger.debug('run() *Entered*')
+
         parser = argparse.ArgumentParser(prog ='orpsoc.py sim '+self.system.name)
         for name, core in self.system.get_cores().items():
             if core.plusargs:
@@ -68,4 +83,3 @@ class Simulator(object):
             else:
                 self.plusargs += [key+'='+str(value[0])]
         print(self.plusargs)
-        
