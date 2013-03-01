2c90b64aac3d07e5688b6a05957898496f6b9a73
Olof Kindgren <olof@opencores.org>
2013-03-01 23:03:45 +0100

    Remove unused FetchSvn.py

--- orpsoc/FetchSvn.py
+++
@@ -1,9 +0,0 @@
-import pysvn
-
-client = pysvn.Client()
-repo_root  = 'http://opencores.org/ocsvn/openrisc/openrisc'
-repo_path  = 'trunk/or1200'
-local_path = 'or1200_675'
-client.checkout(os.path.join(repo_root,repo_path),
-                local_path
-                revision=675)
