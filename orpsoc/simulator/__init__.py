a00ab0ba9d3d61e8a270458aa27206e4c2435eb9
Olof Kindgren <olof@opencores.org>
2013-08-14 23:42:32 +0200

    simulator: Register verilator in SimulatorFactory

--- orpsoc/simulator/__init__.py
+++ orpsoc/simulator/__init__.py
@@ -1,9 +1,12 @@
 from orpsoc.simulator.icarus import SimulatorIcarus
 from orpsoc.simulator.modelsim import Modelsim
+from orpsoc.simulator.verilator import Verilator
 def SimulatorFactory(sim,system):
     if sim == 'icarus':
         return SimulatorIcarus(system)
     elif sim == 'modelsim':
         return Modelsim(system)
+    elif sim == 'verilator':
+        return Verilator(system)
     else:
         raise Exception
