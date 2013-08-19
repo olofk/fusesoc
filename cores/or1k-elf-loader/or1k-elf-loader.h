d0d48f452771dbd2a824caa79844e4567214d91c
Olof Kindgren <olof@opencores.org>
2013-08-19 21:15:51 +0200

    Moved cores and systems to a separate repo

--- cores/or1k-elf-loader/or1k-elf-loader.h
+++
@@ -1,5 +0,0 @@
-#include <stdio.h>
-
-FILE* load_elf_file(char* elf_file_name);
-long get_size(FILE* bin_file);
-unsigned int read_32(FILE* bin_file, unsigned int address);
--- systems/or1200-generic/bench/verilator/or1k-elf-loader.h
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-FILE* load_elf_file(char* elf_file_name);
-long get_size(FILE* bin_file);
-unsigned int read_32(FILE* bin_file, unsigned int address);
-unsigned short read_16(FILE* bin_file, unsigned int address);
