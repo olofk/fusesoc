a9e2d385d503e01d1cf5f737b52f913564f65012
Olof Kindgren <olof@opencores.org>
2013-10-31 20:46:42 +0100

    config.py: Look for global config files and use XDG dirs
    orpsoc now tries to read from /etc/orpsoc/orpsoc.conf,
    $XDG_CONFIG_HOME/orpsoc/orpsoc.conf (XDG_CONFIG_HOME defaults to ~/.config)
    and finally orpsoc.conf in cwd. Options in each file overwrite the previous
    value. Cache location now defaults to $XDG_CACHE_HOME/orpsoc
    (XDG_CACHE_HOME defaults to ~/.cache) unless explicitly overridden with
    cache_root in any of the config files

--- orpsoc/config.py
+++ orpsoc/config.py
@@ -1,3 +1,5 @@
+import logging
+
 import sys
 if sys.version[0] == '2':
     import ConfigParser as configparser
@@ -6,11 +8,8 @@ else:
 
 import os
 
-DEFAULT_VALUES = {'prefix'       : os.getcwd(),
-                  'build_root'   : '%(prefix)s/build',
-                  'cache_root'   : '%(prefix)s/cache',
-                  'cores_root'   : '%(prefix)s/cores',
-                  'systems_root' : '%(prefix)s/systems'}
+logger = logging.getLogger(__name__)
+
 class Config(object):
     _instance = None
 
@@ -20,26 +19,53 @@ class Config(object):
         return cls._instance
 
     def __init__(self):
-        #FIXME: Don't hardcode ./orpsoc.conf
+        #TODO: Add option to load custom config file
         self.build_root = None
         self.cache_root = None
-        self.cores_root = None
+        self.cores_root = []
         self.systems_root = None
-        config_file = './orpsoc.conf'
-        if os.path.exists(config_file):
-            config = configparser.SafeConfigParser(DEFAULT_VALUES)
-            config.readfp(open('./orpsoc.conf'))
 
-            self.build_root = config.get('main','build_root')
-            self.cache_root = config.get('main','cache_root')
-            self.cores_root = config.get('main','cores_root')
-            self.systems_root = config.get('main','systems_root')
+        xdg_config_home = os.environ.get('XDG_CONFIG_HOME') or \
+                          os.path.join(os.path.expanduser('~'), '.config')
+        config = configparser.SafeConfigParser()
+        config_files = ['/etc/orpsoc/orpsoc.conf',
+                        os.path.join(xdg_config_home, 'orpsoc','orpsoc.conf'),
+                        'orpsoc.conf']
 
+        logger.debug('Looking for config files from ' + ':'.join(config_files))
+        files_read = config.read(config_files)
+        logger.debug('Found config files in ' + ':'.join(files_read))
+
+        for item in ['build_root', 'cache_root', 'systems_root']:
+            try:
+                setattr(self, item, config.get('main', item))
+            except configparser.NoOptionError:
+                pass
+            except configparser.NoSectionError:
+                pass
+        item = 'cores_root'
+        try:
+            setattr(self, item, config.get('main', item).split())
+        except configparser.NoOptionError:
+            pass
+        except configparser.NoSectionError:
+            pass
+
+        #Set fallback values
         if self.build_root is None:
-            self.build_root   = 'build'
+            self.build_root   = os.path.abspath('build')
         if self.cache_root is None:
-            self.cache_root   = 'cache'
+            xdg_cache_home = os.environ.get('XDG_CACHE_HOME') or \
+                             os.path.join(os.path.expanduser('~'), '.cache')
+            self.cache_root = os.path.join(xdg_cache_home, 'orpsoc')
+            if not os.path.exists(self.cache_root):
+                os.makedirs(self.cache_root)
         if self.cores_root is None and os.path.exists('cores'):
-            self.cores_root   = 'cores'
+            self.cores_root   = [os.path.abspath('cores')]
         if self.systems_root is None and os.path.exists('systems'):
-            self.systems_root = 'systems'
+            self.systems_root = os.path.abspath('systems')
+
+        logger.debug('build_root='+self.build_root)
+        logger.debug('cache_root='+self.cache_root)
+        logger.debug('cores_root='+':'.join(self.cores_root))
+        logger.debug('systems_root='+self.systems_root if self.systems_root else "Not defined")
