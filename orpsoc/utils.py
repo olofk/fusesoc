2b4b01e45e1599ef7964fdbce9c2e5d552b49c81
Olof Kindgren <olof@opencores.org>
2014-01-10 23:32:37 +0100

    utils.py: Add Launcher class
    Launcher is a wrapper class around subprocess.check_call that aims
    to simplify code for calling external programs

--- orpsoc/utils.py
+++ orpsoc/utils.py
@@ -1,5 +1,33 @@
 import subprocess
 
+class Launcher:
+    def __init__(self, cmd, args=[], shell=False, cwd=None, stderr=None, errormsg=None):
+        self.cmd      = cmd
+        self.args     = args
+        self.shell    = shell
+        self.cwd      = cwd
+        self.stderr   = stderr
+        self.errormsg = errormsg
+
+    def run(self):
+        try:
+            subprocess.check_call([self.cmd] + self.args,
+                                  cwd = self.cwd,
+                                  shell = self.shell,
+                                  stderr = self.stderr),
+        except OSError:
+            raise RunTimeError("Error: Command " + self.cmd + " not found. Make sure it is in $PATH")
+        except subprocess.CalledProcessError:
+            if self.stderr is None:
+                self.stderr = "stderr"
+                if self.errormsg:
+                    raise RunTimeError(errormsg)
+                else:
+                    raise RunTimeError("Error: " + self.cmd + ' '.join(args) + " returned errors. See " + self.stderr + " for details")
+
+    def __str__(self):
+        return self.cmd + ' ' + ' '.join(self.args)
+        
 def launch(cmd, args=[], shell=False, cwd=None, stderr=None):
     try:
         subprocess.check_call([cmd] + args,
