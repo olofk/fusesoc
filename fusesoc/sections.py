21cd2cac147d1695afb880cd679289656cf03122
Olof Kindgren <olof.kindgren@gmail.com>
2014-03-14 22:17:20 +0100

    sections.py: Add __str__ function for sections

--- fusesoc/sections.py
+++ fusesoc/sections.py
@@ -23,6 +23,14 @@ class Section(object):
             else:
                 print("Warning: Unknown item '{item}' in section '{section}'".format(item=item, section=self.name))
 
+    def __str__(self):
+        s = ''
+        for item in self.lists:
+            s += item + ' : ' + ';'.join(getattr(self, item)) + '\n'
+        for item in self.strings:
+            s += item + ' : ' + getattr(self, item) + '\n'
+        return s
+
 class ToolSection(Section):
     def __init__(self):
         super(ToolSection, self).__init__()
@@ -70,3 +78,22 @@ class VerilatorSection(ToolSection):
         if items:
             self.load_dict(items)
             self.include_dirs  = list(set(map(os.path.dirname, self.include_files)))
+
+    def __str__(self):
+        s = """Verilator options       : {verilator_options}
+Testbench source files  : {src_files}
+Testbench include files : {include_files}
+Testbench define files  : {define_files}
+External libraries      : {libs}
+Testbench top level     : {tb_toplevel}
+Testbench source type   : {source_type}
+Verilog top module      : {top_module}
+"""
+        return s.format(verilator_options=' '.join(self.verilator_options),
+                        src_files = ' '.join(self.src_files),
+                        include_files=' '.join(self.include_files),
+                        define_files=' '.join(self.define_files),
+                        libs=' '.join(self.libs),
+                        tb_toplevel=self.tb_toplevel,
+                        source_type=self.source_type,
+                        top_module=self.top_module)
