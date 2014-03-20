e9c331e9b1fc3446f59b65f124fd81a14df64ce1
Franck Jullien <franck.jullien@gmail.com>
2014-03-20 22:00:24 +0100

    provider: raise exception on unknown/missing provider
    Signed-off-by: Franck Jullien <franck.jullien@gmail.com>

--- fusesoc/provider/__init__.py
+++ fusesoc/provider/__init__.py
@@ -13,11 +13,14 @@ from fusesoc.provider.github import GitHub
 from fusesoc.provider.url import ProviderURL
 
 def ProviderFactory(items):
-    if items.get('name') == 'opencores':
-        return ProviderOpenCores(dict(items))
-    elif items.get('name') == 'github':
-        return GitHub(dict(items))
-    elif items.get('name') == 'url':
-        return ProviderURL(dict(items))
+    if items.get('name') is not None:
+        if items.get('name') == 'opencores':
+            return ProviderOpenCores(dict(items))
+        elif items.get('name') == 'github':
+            return GitHub(dict(items))
+        elif items.get('name') == 'url':
+            return ProviderURL(dict(items))
+        else:
+            raise RuntimeError("Unknown provider '" + items.get('name') + "' in section [provider]")
     else:
-        raise Exception('Unknown provider')
+        raise RuntimeError("Missing 'name' in section [provider]")
