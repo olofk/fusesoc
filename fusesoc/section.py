094643931ca7f4de97e9572b7891a0793e7baed6
Stefan Wallentowitz <stefan@wallentowitz.de>
2016-07-17 07:49:43 +0200

    Add Xilinx Vivado backend and rework XSIM
    This adds the Vivado backend. It uses the project workflow to generate
    a project file, generate IP cores etc.
    
    The XSIM simulation is reworked accordingly, the overlap can in future
    be put in common subroutines (but did not find _the_ nice solution so
    far).

--- fusesoc/section.py
+++ fusesoc/section.py
@@ -364,14 +364,16 @@ class XsimSection(ToolSection):
     def __init__(self, items=None):
         super(XsimSection, self).__init__()
 
-        self._add_member('xsim_options', StringList, "Extra Xsim compile options")
+        self._add_member('top_module' , str, 'verilog top-level module')
+        self._add_member('part', str, 'FPGA part')
 
         if items:
             self.load_dict(items)
 
     def __str__(self):
         s = super(XsimSection, self).__str__()
-        if self.xsim_options: s += "Xsim compile options : {}\n".format(' '.join(self.xsim_options))
+        if self.top_module: s += "top module : {}\n".format(' '.join(self.top_module))
+        if self.part: s += "part : {}\n".format(' '.join(self.part))
         return s
 
 class VerilatorSection(ToolSection):
@@ -439,6 +441,19 @@ class IcestormSection(ToolSection):
         if items:
             self.load_dict(items)
             self.export_files = self.pcf_file
+
+class VivadoSection(ToolSection):
+
+    TAG = 'vivado'
+
+    def __init__(self, items=None):
+        super(VivadoSection, self).__init__()
+
+        self._add_member('part'       , str, 'FPGA device part')
+        self._add_member('hw_device'  , str, 'FPGA device identifier')
+
+        if items:
+            self.load_dict(items)
 
 class IseSection(ToolSection):
 
