9ef09a110a330ce9216337f09c06c4226ed7225e
Olof Kindgren <olof.kindgren@gmail.com>
2016-11-04 23:45:52 +0100

    Add workaround for broken dependency handler in pip
    sat-solver (a dependency of FuseSoC) requrires attrs >= 15.2.0
    okonomyaki (a dependency of sat-solver) requires attrs <= 16.1.0
    
    A bug in pip makes it ignore the second requirement, installs
    latest attrs, and then fails to install okonomiyaki.
    
    We work around this by explicitly require attrs == 16.0.0 as a
    dependency of FuseSoC

--- setup.py
+++ setup.py
@@ -30,6 +30,7 @@ setup(
         ]
     },
     install_requires=[
+          'attrs==16.0.0', #Workaround for broken pip dep handler
           'ipyxact>=0.2.3',
           'simplesat>=0.4.0',
     ],
