b8f441a7fe3210926e47776cb2bca536c3382604
Antony Pavlov <antonynpavlov@gmail.com>
2016-12-18 10:31:14 +0300

    build: quartus.py: fix pylint whitespace warnings
    Signed-off-by: Antony Pavlov <antonynpavlov@gmail.com>

--- fusesoc/build/quartus.py
+++ fusesoc/build/quartus.py
@@ -41,7 +41,7 @@ clean:
         if not self.backend.qsys_files:
             return
 
-        qsys_script = open(os.path.join(self.work_root, 'qsys.sh'),'w')
+        qsys_script = open(os.path.join(self.work_root, 'qsys.sh'), 'w')
 
         for f in self.backend.qsys_files:
             dst_file = os.path.join(self.work_root, f.name)
@@ -90,7 +90,7 @@ clean:
         subprocess.call(['sh', os.path.join(self.work_root, 'qsys.sh')])
 
     def _write_tcl_file(self):
-        tcl_file = open(os.path.join(self.work_root, self.system.sanitized_name+'.tcl'),'w')
+        tcl_file = open(os.path.join(self.work_root, self.system.sanitized_name+'.tcl'), 'w')
         tcl_file.write("project_new " + self.system.sanitized_name + " -overwrite\n")
         tcl_file.write("set_global_assignment -name FAMILY " + self.backend.family + '\n')
         tcl_file.write("set_global_assignment -name DEVICE " + self.backend.device + '\n')
@@ -106,14 +106,14 @@ clean:
 
         for f in src_files:
             if f.file_type in ["verilogSource",
-		               "verilogSource-95",
+                               "verilogSource-95",
                                "verilogSource-2001",
-		               "verilogSource-2005"]:
+                               "verilogSource-2005"]:
                 _type = 'VERILOG_FILE'
             elif f.file_type in ["systemVerilogSource",
-			         "systemVerilogSource-3.0",
-			         "systemVerilogSource-3.1",
-			         "systemVerilogSource-3.1a"]:
+                                 "systemVerilogSource-3.0",
+                                 "systemVerilogSource-3.1",
+                                 "systemVerilogSource-3.1a"]:
                 _type = 'SYSTEMVERILOG_FILE'
             elif f.file_type in ['vhdlSource',
                                  'vhdlSource-87',
@@ -125,7 +125,7 @@ clean:
             elif f.file_type in ['SDC']:
                 _type = 'SDC_FILE'
             elif f.file_type in ['tclSource']:
-                tcl_file.write("source {}\n".format(f.name.replace('\\','/')))
+                tcl_file.write("source {}\n".format(f.name.replace('\\', '/')))
                 _type = None
             elif f.file_type in ['user']:
                 _type = None
@@ -133,26 +133,26 @@ clean:
                 _type = None
                 _s = "{} has unknown file type '{}'"
                 utils.pr_warn(_s.format(f.name,
-                                  f.file_type))
+                                        f.file_type))
             if _type:
                 _s = "set_global_assignment -name {} {}\n"
                 tcl_file.write(_s.format(_type,
-                                         f.name.replace('\\','/')))
+                                         f.name.replace('\\', '/')))
 
         for include_dir in incdirs:
-            tcl_file.write("set_global_assignment -name SEARCH_PATH " + include_dir.replace('\\','/') + '\n')
+            tcl_file.write("set_global_assignment -name SEARCH_PATH " + include_dir.replace('\\', '/') + '\n')
 
         for f in self.backend.sdc_files:
             dst_dir = os.path.join(self.src_root, self.system.sanitized_name)
-            sdc_file = os.path.relpath(os.path.join(dst_dir, f.name) , self.work_root)
-            tcl_file.write("set_global_assignment -name SDC_FILE " + sdc_file.replace('\\','/') + '\n')
+            sdc_file = os.path.relpath(os.path.join(dst_dir, f.name), self.work_root)
+            tcl_file.write("set_global_assignment -name SDC_FILE " + sdc_file.replace('\\', '/') + '\n')
 
         # NOTE: The relative path _have_ to be used here, if the absolute path
         # is used, quartus_asm will fail with an error message that
         # sdram_io.pre.h can't be read or written.
         for f in self.qip_files:
             tcl_file.write("set_global_assignment -name QIP_FILE " +
-                           os.path.relpath(f, self.work_root).replace('\\','/') + '\n')
+                           os.path.relpath(f, self.work_root).replace('\\', '/') + '\n')
 
         tcl_files = self.backend.tcl_files
         for f in tcl_files:
@@ -161,7 +161,7 @@ clean:
 
     def _write_makefile(self):
         quartus_options = self.backend.quartus_options
-        makefile = open(os.path.join(self.work_root, 'Makefile'),'w')
+        makefile = open(os.path.join(self.work_root, 'Makefile'), 'w')
         makefile.write("DESIGN_NAME = " + self.system.sanitized_name + "\n")
         makefile.write("QUARTUS_OPTIONS = " + quartus_options + "\n")
         makefile.write(self.MAKEFILE_TEMPLATE)
@@ -170,7 +170,7 @@ clean:
     def build(self, args):
         super(Quartus, self).build(args)
 
-        utils.Launcher('make', cwd = self.work_root).run()
+        utils.Launcher('make', cwd=self.work_root).run()
 
         super(Quartus, self).done()
 
