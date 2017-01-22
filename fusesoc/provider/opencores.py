557613859818f45265eb7730c57b6ee6eefb002c
William D. Jones <thor0505@comcast.net>
2017-01-22 03:37:12 -0500

    Add cygpath handling to opencores provider.

--- fusesoc/provider/opencores.py
+++ fusesoc/provider/opencores.py
@@ -1,7 +1,9 @@
 import logging
+import sys
 
 from fusesoc.provider.provider import Provider
-from fusesoc.utils import Launcher
+from fusesoc.utils import Launcher, cygpath
+from fusesoc.config import Config
 
 logger = logging.getLogger(__name__)
 
@@ -16,6 +18,9 @@ class Opencores(Provider):
                                      self.config.get('repo_root'))
         revision_number  = self.config.get('revision')
         logger.info("Downloading " + repo_name + " from OpenCores")
+
+        if sys.platform == "win32" and Config().cygpath:
+            local_dir = cygpath(local_dir)
 
         Launcher('svn', ['co', '-q', '--no-auth-cache',
                          '-r', revision_number,
