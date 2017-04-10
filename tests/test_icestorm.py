95732a574be876cc15b9b2f758937f5a82a677a3
Olof Kindgren <olof.kindgren@gmail.com>
2017-04-10 10:03:20 +0200

    Add tests for icestorm backend

---
+++ tests/test_icestorm.py
@@ -0,0 +1,29 @@
+import difflib
+import os
+import pytest
+
+from fusesoc.main import _import
+from test_common import compare_file, get_core, vlogdefines, vlogparams
+
+tests_dir = os.path.dirname(__file__)
+params = vlogparams + vlogdefines
+core = get_core("c3demo")
+backend =_import('build', core.main.backend)(core, export=False)
+ref_dir = os.path.join(tests_dir, __name__)
+work_root = backend.work_root
+
+def test_icestorm_configure():
+
+    backend.configure(params)
+
+    ys_file = core.name.sanitized_name + '.ys'
+
+    assert '' == compare_file(ref_dir, work_root, "Makefile")
+    assert '' == compare_file(ref_dir, work_root, "config.mk")
+    assert '' == compare_file(ref_dir, work_root, ys_file)
+
+def test_icestorm_build():
+    os.environ['PATH'] = os.path.join(tests_dir, 'mock_commands')+':'+os.environ['PATH']
+    backend.build(params)
+
+    assert '' == compare_file(ref_dir, work_root, 'run.cmd')
