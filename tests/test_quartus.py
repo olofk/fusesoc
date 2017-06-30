cccf2e12bfc31a9e60ef1761a0fcacd15e19e3ba
Olof Kindgren <olof.kindgren@gmail.com>
2017-06-30 23:56:11 +0200

    Improve quartus testcases

--- tests/test_quartus.py
+++ tests/test_quartus.py
@@ -2,18 +2,31 @@ import difflib
 import os
 import pytest
 
-from test_common import compare_file, get_core, get_synth, vlogdefines, vlogparams
+from test_common import get_core, get_synth, vlogdefines, vlogparams
 
-def test_quartus():
-    tests_dir = os.path.dirname(__file__)
+tests_dir = os.path.dirname(__file__)
+core = get_core("sockit")
+backend = get_synth('quartus', core)
+work_root = backend.work_root
+
+def test_quartus_configure():
     params = vlogparams + vlogdefines
 
-    core = get_core("sockit")
-
-    backend = get_synth('quartus', core)
     backend.configure(params)
 
     tcl_file = core.name.sanitized_name + '.tcl'
     ref_dir = os.path.join(tests_dir, __name__)
 
-    assert '' == compare_file(ref_dir, backend.work_root, tcl_file)
+    for f in ['config.mk',
+              'Makefile',
+              'sockit_0.tcl']:
+        with open(os.path.join(ref_dir, f)) as fref, open(os.path.join(work_root, f)) as fgen:
+            assert fref.read() == fgen.read(), f
+
+def test_quartus_build():
+    os.environ['PATH'] = os.path.join(tests_dir, 'mock_commands')+':'+os.environ['PATH']
+    backend.build(None)
+
+def test_quartus_pgm():
+    os.environ['PATH'] = os.path.join(tests_dir, 'mock_commands')+':'+os.environ['PATH']
+    backend.pgm([])
