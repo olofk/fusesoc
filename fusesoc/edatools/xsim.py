6544db0e297eec22eb3c28a190a1595fb7f8e531
bqwer <liventsev@gmail.com>
2017-09-28 21:26:37 +0300

    dat files need to be source

--- fusesoc/edatools/xsim.py
+++ fusesoc/edatools/xsim.py
@@ -2,6 +2,8 @@ import os
 from .simulator import Simulator
 import logging
 from fusesoc.utils import Launcher
+import time
+from shutil import copyfile
 
 logger = logging.getLogger(__name__)
 
@@ -9,6 +11,7 @@ class Xsim(Simulator):
 
     def configure(self, args):
         super(Xsim, self).configure(args)
+        self._generate_xci_sim_scripts()
         self._write_config_files()
 
     def _write_config_files(self):
@@ -35,6 +38,8 @@ class Xsim(Simulator):
                                         "systemVerilogSource-3.1a",
                                         "verilogSource-2005"]:
                 f1.write('sv work ' + src_file.name + '\n')
+            elif src_file.file_type in ["xci"]:
+                pass
             else:
                 _s = "{} has unknown file type '{}'"
                 logger.warning(_s.format(src_file.name, src_file.file_type))
@@ -46,7 +51,80 @@ class Xsim(Simulator):
         f2.write('run all\n')
         f2.close()
 
+    def _generate_xci_sim_scripts(self):
+        # make project from all xci
+        (src_files, self.incdirs) = self._get_fileset_files()
+        xci_ip = []
+        for src_file in src_files:
+            if src_file.file_type == 'xci':
+                xci_ip.append(src_file.name);
+
+        if len(xci_ip)>0:
+            tcl_file = open(os.path.join(self.work_root, 'xci_sim.tcl'), 'w')
+            ipconfig = 'create_project ' + self.name + '_xci_sim\n'
+            ipconfig += 'set_property \"simulator_language\" \"Mixed\" [current_project]\n'
+            ipconfig += '\n'.join(['read_ip '+os.path.join(self.work_root, s) for s in xci_ip])+'\n'
+            ipconfig += 'upgrade_ip [get_ips]\n'
+            #  ipconfig += '\n'.join(['generate_target simulation [get_files '+os.path.basename(s)+']'\
+            ipconfig += '\n'.join(['generate_target all [get_files '+ os.path.join(self.work_root, s) +']' for s in xci_ip]) + '\n'
+            ipconfig += '\n'.join(['export_ip_user_files -of_objects [get_files '+ os.path.join(self.work_root, s) +'] -no_script -ip_user_files_dir '+ self.work_root + ' -force -quiet' for s in xci_ip]) + '\n'
+            ipconfig += '\n'.join(['create_ip_run [get_files ' + os.path.join(self.work_root, s) + ']' for s in xci_ip]) + '\n'
+            ipconfig += '\n'.join(['launch_runs -dir ' +self.work_root +' ' +os.path.splitext(os.path.basename(s))[0]+'_synth_1' for s in xci_ip]) + '\n'
+            ipconfig += '\n'.join(['wait_on_run '+os.path.splitext(os.path.basename(s))[0]+'_synth_1' for s in xci_ip]) + '\n'
+            ipconfig += '\n'.join(['export_simulation -directory ' + self.work_root +\
+                        ' -simulator xsim -of_objects [get_files '+ os.path.join(self.work_root, s) +'] -ip_user_files_dir ' + self.work_root + ' -force -quiet' for s in xci_ip])
+            tcl_file.write(ipconfig)
+
+        tcl_file = 'xsim.tcl'
+        f2 = open(os.path.join(self.work_root,tcl_file),'w')
+        f2.write('add_wave -radix hex /\n')
+        f2.write('run all\n')
+        f2.close()
+
+        tcl_file_name = os.path.join(self.work_root, 'xci_sim.tcl')
+        if (os.path.isfile(tcl_file_name)):
+            Launcher('vivado', ['-mode', 'batch', '-source', tcl_file_name],
+                               cwd = self.work_root,
+                               errormsg = "Failed to generate simulation scripts from xci").run()
+
+
     def build_main(self):
+
+        #  tcl_file_name = os.path.join(self.work_root, 'xci_sim.tcl')
+        #  if (os.path.isfile(tcl_file_name)):
+        #      Launcher('vivado', ['-mode', 'batch', '-source', tcl_file_name],
+        #                         cwd = self.work_root,
+        #                         errormsg = "Failed to generate simulation scripts from xci").run()
+
+        (src_files, self.incdirs) = self._get_fileset_files()
+        for src_file in src_files:
+            if src_file.file_type == 'xci':
+                ip_name = os.path.splitext(os.path.basename(src_file.name))[0]
+                vlog_prj  = os.path.join(self.work_root, ip_name , 'xsim', 'vlog.prj')
+                vhdl_prj  = os.path.join(self.work_root, ip_name , 'xsim', 'vhdl.prj')
+                glbl_file = os.path.join(self.work_root, ip_name , 'xsim', 'glbl.v')
+
+                if os.path.isfile(vlog_prj):
+                    Launcher('xvlog', ['--prj', vlog_prj],
+                             cwd      = self.work_root,
+                             errormsg = "Failed to compile Xsim simulation model").run()
+
+                if os.path.isfile(vhdl_prj):
+                    Launcher('xvhdl', ['--prj', vhdl_prj],
+                             cwd      = self.work_root,
+                             errormsg = "Failed to compile Xsim simulation model").run()
+
+                if os.path.isfile(vlog_prj):
+                    Launcher('xvlog', [glbl_file],
+                             cwd      = self.work_root,
+                             errormsg = "Failed to compile Xsim simulation model").run()
+
+        # .dat should be src_file for sim
+        #  for src_file in src_files:
+        #      print(src_file.name)
+        #      if os.path.splitext(src_file.name)[1] == 'dat':
+        #          copyfile(src_file.name, os.path.join(self.work_root, os.path.basename(src_file.name)))
+
         #Check if any VPI modules are present and display warning
         if len(self.vpi_modules) > 0:
             modules = [m['name'] for m in self.vpi_modules]
@@ -55,10 +133,30 @@ class Xsim(Simulator):
         #Build simulation model
         args = []
         args += [ self.toplevel]
+        args += [ 'glbl' ]
         args += ['--prj', 'xsim.prj']      # list of design files
         args += ['--timescale', '1ps/1ps'] # default timescale to prevent error if unspecified
         args += ['--snapshot', 'fusesoc']  # name of the design to simulate
         args += ['--debug', 'typical']     # capture waveforms
+        args += ['--relax']     # capture waveforms
+
+        #Get IP libraries
+        libs=[]
+        for src_file in src_files:
+            if src_file.file_type == 'xci':
+                ip_name = os.path.splitext(os.path.basename(src_file.name))[0]
+                ini_file = os.path.join(self.work_root, ip_name ,'xsim' , 'xsim.ini')
+                with open(ini_file, 'r') as f:
+                    for line in f:
+                        lib_name = line.split('=')[0]
+                        if lib_name not in libs:
+                            args += ['-L', lib_name]
+                            libs.append(lib_name)
+
+        args += ['-L', 'work']
+        args += ['-L', 'unisims_ver']
+        args += ['-L', 'unimacro_ver']
+        args += ['-L', 'secureip']
 
         for include_dir in self.incdirs:
             args += ['-i', include_dir]
@@ -66,8 +164,8 @@ class Xsim(Simulator):
         for key, value in self.vlogparam.items():
             args += ['--generic_top', '{}={}'.format(key, self._param_value_str(value))]
 
-        if 'xsim_options' in self.tool_options:
-            args += self.tool_options['xsim_options']
+        #  if 'xsim_options' in self.tool_options:
+        #      args += self.tool_options['xsim_options']
 
         Launcher('xelab', args,
                  cwd      = self.work_root,
@@ -88,8 +186,10 @@ class Xsim(Simulator):
             args += ['--testplusarg', '{}={}'.format(key, value)]
         #FIXME Top-level parameters
 
+
         Launcher('xsim', args,
                  cwd = self.work_root,
                  errormsg = "Failed to run Xsim simulation").run()
 
         super(Xsim, self).done(args)
+
