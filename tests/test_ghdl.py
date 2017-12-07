17f4aa1e71bc6ae44a720ca96a13af1311f33990
Olof Kindgren <olof.kindgren@gmail.com>
2017-12-07 13:27:59 +0100

    Rewrite ghdl backend

---
+++ tests/test_ghdl.py
@@ -0,0 +1,32 @@
+import os
+import pytest
+
+from test_common import compare_files, get_core, get_sim, sim_params
+
+tests_dir = os.path.dirname(__file__)
+core      = get_core("ghdltest")
+backend   = get_sim('ghdl', core)
+ref_dir   = os.path.join(tests_dir, __name__)
+work_root = backend.work_root
+
+def test_ghdl_configure():
+
+    backend.configure(sim_params)
+
+    compare_files(ref_dir, work_root, ['Makefile'])
+
+def test_ghdl_build():
+
+    os.environ['PATH'] = os.path.join(tests_dir, 'mock_commands')+':'+os.environ['PATH']
+
+    backend.build()
+    assert os.path.isfile(os.path.join(work_root, 'pre_build_script_executed'))
+
+def test_ghdl_run():
+
+    os.environ['PATH'] = os.path.join(tests_dir, 'mock_commands')+':'+os.environ['PATH']
+    backend.run(sim_params)
+
+    compare_files(ref_dir, work_root, ['run.cmd'])
+    assert os.path.isfile(os.path.join(work_root, 'pre_run_script_executed'))
+    assert os.path.isfile(os.path.join(work_root, 'post_run_script_executed'))
---
+++ tests/test_ghdl/run.cmd
@@ -0,0 +1,1 @@
+run EXTRA_OPTIONS= -gvlogparam_bool=1 -gvlogparam_int=42 -gvlogparam_str="hello"
