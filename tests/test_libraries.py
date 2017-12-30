ce9748f4da1a404bc77069051127dd982ac27a56
Andrew Wygle <awygle@gmail.com>
2017-12-30 13:05:22 -0800

    Added support for updating libraries, respecting auto-sync

--- tests/test_libraries.py
+++ tests/test_libraries.py
@@ -1,10 +1,12 @@
 import tempfile
+import shutil
 import os.path
+import logging
+import subprocess
 
 from argparse import Namespace
 
 from fusesoc.config import Config
-from fusesoc.main import run
 
 from test_common import cores_root, build_root, cache_root, library_root
 
@@ -16,9 +18,11 @@ library_root = {library_root}
 
 [library.test_lib]
 location = {cores_root}
-auto-sync = false
-sync-uri = https://github.com/fusesoc/fusesoc-cores
+auto-sync = {auto_sync}
+sync-uri = {sync_uri}
 """
+
+sync_uri = 'https://github.com/fusesoc/fusesoc-cores'
 
 def _run_test_util(cm, args):
     from fusesoc.main import _get_core
@@ -26,28 +30,96 @@ def _run_test_util(cm, args):
     _get_core(cm, 'atlys')
 
 def test_library_location():
+    from fusesoc.main import run
+
     tcf = tempfile.TemporaryFile(mode="w+")
     tcf.write(EXAMPLE_CONFIG.format(
             build_root = build_root,
             cache_root = cache_root,
             cores_root = cores_root,
-            library_root = library_root
+            library_root = library_root,
+            auto_sync = 'false',
+            sync_uri = sync_uri
             )
         )
     tcf.seek(0)
-    
+
     conf = Config(file=tcf)
-    
+
     args = Namespace()
-    
+
     # TODO find a better way to set up these defaults
     args.verbose = False
     args.monochrome = False
     args.cores_root = []
     vars(args)['32'] = False
     vars(args)['64'] = False
-    
+
     args.config = tcf
     args.func = _run_test_util
-    
+
     run(args)
+
+def test_library_update(caplog):
+    from fusesoc.main import update, run
+
+    clone_target = tempfile.mkdtemp()
+
+    try:
+        subprocess.call(['git', 'clone', sync_uri, clone_target])
+
+        tcf = tempfile.TemporaryFile(mode="w+")
+        tcf.write(EXAMPLE_CONFIG.format(
+                build_root = build_root,
+                cache_root = cache_root,
+                cores_root = clone_target,
+                library_root = library_root,
+                auto_sync = 'false',
+                sync_uri = sync_uri
+                )
+            )
+        tcf.seek(0)
+
+        conf = Config(file=tcf)
+
+        args = Namespace()
+
+        # TODO find a better way to set up these defaults
+        args.verbose = False
+        args.monochrome = False
+        args.cores_root = []
+        vars(args)['32'] = False
+        vars(args)['64'] = False
+
+        args.config = tcf
+        args.libraries = []
+        args.func = update
+
+        with caplog.at_level(logging.INFO):
+            run(args)
+
+        assert not "Updating 'test_lib'" in caplog.text
+
+        caplog.clear()
+
+        args.libraries = ['test_lib']
+
+        with caplog.at_level(logging.INFO):
+            run(args)
+
+        assert "Updating 'test_lib'" in caplog.text
+
+        caplog.clear()
+
+        args.libraries = []
+        conf.libraries['test_lib']['auto-sync'] = True
+
+        with caplog.at_level(logging.INFO):
+            run(args)
+
+        assert "Updating 'test_lib'" in caplog.text
+
+        caplog.clear()
+
+    finally:
+        shutil.rmtree(clone_target)
