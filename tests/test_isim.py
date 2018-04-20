78c5580d349d6bec0715a36c13437177a726f7ad
Olof Kindgren <olof.kindgren@gmail.com>
2018-04-20 22:46:05 +0200

    Reduce code duplication in isim test

--- tests/test_isim.py
+++ tests/test_isim.py
@@ -3,29 +3,20 @@ import pytest
 def test_isim():
     import os
     import shutil
-    import tempfile
-    import yaml
-    from fusesoc.edatools import get_edatool
-    from edalize_common import compare_files, files, param_gen, tests_dir, vpi
+    from edalize_common import compare_files, setup_backend, tests_dir
 
-    (parameters, args) = param_gen(['plusarg', 'vlogdefine', 'vlogparam'])
+    ref_dir      = os.path.join(tests_dir, __name__)
+    paramtypes   = ['plusarg', 'vlogdefine', 'vlogparam']
+    name         = 'test_isim_0'
+    tool         = 'isim'
+    tool_options = {
+        'fuse_options' : ['some', 'fuse_options'],
+        'isim_options' : ['a', 'few', 'isim_options'],
+    }
 
-    work_root = tempfile.mkdtemp(prefix='isim_')
-    eda_api_file = os.path.join(work_root, 'test_isim_0.eda.yml')
-    with open(eda_api_file,'w') as f:
-        f.write(yaml.dump({'name'       : 'test_isim_0',
-                           'files'      : files,
-                           'parameters' : parameters,
-                           'tool_options' : {'isim' : {
-                               'fuse_options' : ['some', 'fuse_options'],
-                               'isim_options' : ['a', 'few', 'isim_options']}},
-                           'toplevel'   : 'top_module',
-                           'vpi'        :  vpi}))
-
-    backend = get_edatool('isim')(eda_api_file=eda_api_file)
+    (backend, args, work_root) = setup_backend(paramtypes, name, tool, tool_options)
     backend.configure(args)
 
-    ref_dir = os.path.join(tests_dir, __name__)
     compare_files(ref_dir, work_root,
                   ['config.mk',
                    'Makefile',
