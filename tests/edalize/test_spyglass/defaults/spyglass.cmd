4373f25bbcad5c2ce5de47334bb1398276cd596d
Philipp Wagner <philipp.wagner@tum.de>
2018-05-22 10:09:39 +0200

    Add tests for spyglass backend

---
+++ tests/edalize/test_spyglass/defaults/spyglass.cmd
@@ -0,0 +1,2 @@
+-enable_pass_exit_codes -tcl spyglass-run-design_read.tcl
+-enable_pass_exit_codes -tcl spyglass-run-lint_lint_rtl.tcl
---
+++ tests/edalize/test_spyglass/tooloptions/spyglass.cmd
@@ -0,0 +1,3 @@
+-enable_pass_exit_codes -tcl spyglass-run-design_read.tcl
+-enable_pass_exit_codes -tcl spyglass-run-lint_lint_rtl.tcl
+-enable_pass_exit_codes -tcl spyglass-run-some_othergoal.tcl
