a2b5d6d668e1b626a1217fb85c9bc1f0105afe06
euripedes <rocha.euripedes@gmail.com>
2018-07-29 17:39:30 -0300

    Pass the std to ghdl at elaboration phase.
    - Ghdl fails at elaboration if the VHDL source file is analyzed with
    --std=08.
    
    Fix ghdl make file generation and fix tests
    
    - STD was being mixed with analyze_options making the options to be
    passed twice. Adding STD to analyze command fix that.
    - The reference files where changed to reflect the modificaion.

--- tests/edalize/test_ghdl/elab-run.cmd
+++ tests/edalize/test_ghdl/elab-run.cmd
@@ -1,1 +1,1 @@
---elab-run top_module a few run_options -gvlogparam_bool=1 -gvlogparam_int=42 -gvlogparam_str=hello
+--elab-run --std=08 top_module a few run_options -gvlogparam_bool=1 -gvlogparam_int=42 -gvlogparam_str=hello
