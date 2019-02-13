ed8a6b91db327caf881b6bd93f1051740c2189f7
Ben Reynwar <ben@reynwar.net>
2019-02-12 17:49:02 -0700

    Display a more helpful error message when the first line in a core file is invalid.

--- fusesoc/core.py
+++ fusesoc/core.py
@@ -1,5 +1,11 @@
+import logging
+
 from fusesoc.capi1.core import Core as Capi1Core
 from fusesoc.capi2.core import Core as Capi2Core
+
+
+logger = logging.getLogger(__name__)
+
 
 class Core(object):
     
@@ -11,4 +17,9 @@ class Core(object):
             elif first_line == 'CAPI=2:':
                 return Capi2Core(*args, **kwargs)
             else:
+                error_msg = 'The first line of the core file {} must be "CAPI=1" or "CAPI=2:".'.format(args[0])
+                error_msg += '  The first line of this core file is "{}".'.format(first_line)
+                if first_line == 'CAPI=2':
+                    error_msg += '  Just add a colon on the end!'
+                logger.warning(error_msg)
                 raise RuntimeError("Unknown file type")
