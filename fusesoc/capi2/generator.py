883438b9e8151d2dc68fce709fb87209362bd31a
GCHQDeveloper560 <48131108+GCHQDeveloper560@users.noreply.github.com>
2019-07-19 10:05:13 +0100

    Allow generator data to be supplied to the constructor

--- fusesoc/capi2/generator.py
+++ fusesoc/capi2/generator.py
@@ -5,16 +5,17 @@ class Generator(object):
     filesets   = {}
     parameters = {}
     targets    = {}
-    def __init__(self):
-        with open(sys.argv[1]) as f:
-            data = yaml.safe_load(f)
+    def __init__(self, data=None):
+        if data is None:
+            with open(sys.argv[1]) as f:
+                data = yaml.safe_load(f)
 
-            self.config     = data.get('parameters')
-            self.files_root = data.get('files_root')
-            self.vlnv       = data.get('vlnv')
+        self.config     = data.get('parameters')
+        self.files_root = data.get('files_root')
+        self.vlnv       = data.get('vlnv')
 
-            #Edalize decide core_file dir. generator creates file
-            self.core_file = self.vlnv.split(':')[2]+'.core'
+        #Edalize decide core_file dir. generator creates file
+        self.core_file = self.vlnv.split(':')[2]+'.core'
 
     def add_files(self, files, fileset='rtl', targets=['default'], file_type=''):
         if not fileset in self.filesets:
@@ -38,7 +39,7 @@ class Generator(object):
                 self.targets[target]['parameters'] = []
             if not parameter in self.targets[target]['parameters']:
                 self.targets[target]['parameters'].append(parameter)
-            
+
     def write(self):
         with open(self.core_file,'w') as f:
             f.write('CAPI=2:\n')
