e7d624af81cd98af00b5305283cb40d29d92d625
Philipp Wagner <mail@philipp-wagner.com>
2019-12-24 12:16:17 +0000

    Add python_requires to setup.py
    This feature enables pip to prevent the installation if Python is too
    old.

--- setup.py
+++ setup.py
@@ -43,4 +43,6 @@ setup(
         'pyyaml',
         'simplesat>=0.8.0',
     ],
+    # Supported Python versions: 2.7 or 3.5+
+    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, <4',
 )
