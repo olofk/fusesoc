87ec9ae3eab75522cb39a20679190db5312d0ccd
Rupert Swarbrick <rswarbrick@gmail.com>
2020-04-02 15:16:34 +0100

    Remove unused flags argument from Core.get_generators()
    This argument doesn't have any effect, so there's no need to construct
    it. This commit removes the argument from the function and all
    call-sites.
    
    It also removes debug messages from in core.get_generators(). The
    first wasn't massively helpful (printing flags that had no effect),
    and the interesting call site in Edalizer.extract_generators() already
    has a log message which was more helpful because it could also print
    the core name.
    
    The commit moves the second debug message (per generator found) up to
    Edalizer.extract_generators, where it was also duplicated.
    
    There should be no functional change, except to debug messages.

--- fusesoc/coremanager.py
+++ fusesoc/coremanager.py
@@ -320,7 +320,7 @@ class CoreManager:
         generators = {}
         for core in self.db.find():
             if hasattr(core, "get_generators"):
-                _generators = core.get_generators({})
+                _generators = core.get_generators()
                 if _generators:
                     generators[str(core.name)] = _generators
         return generators
