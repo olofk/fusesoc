ec1bbcee37210041662406d17fe25995c78353e1
Ben Reynwar <ben@reynwar.net>
2020-06-15 17:28:17 -0700

    The pipe to the stdin of the Launcher process is removed. The pipe is not used, and it gets in the way of debugging the generator using stdin (i.e. pdb breakpoint in generator).

--- fusesoc/utils.py
+++ fusesoc/utils.py
@@ -33,7 +33,7 @@ class Launcher:
         logger.debug("    " + str(self))
         try:
             subprocess.check_call(
-                map(str, [self.cmd] + self.args), cwd=self.cwd, stdin=subprocess.PIPE
+                map(str, [self.cmd] + self.args), cwd=self.cwd,
             ),
         except FileNotFoundError:
             raise RuntimeError(
