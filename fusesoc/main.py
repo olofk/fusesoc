fe1a5a7a555af7af783863de96f792378f284b34
Philipp Wagner <mail@philipp-wagner.com>
2020-07-27 18:03:05 +0100

    Put shebang before license comment
    It should be the first line in a script.

--- fusesoc/main.py
+++ fusesoc/main.py
@@ -1,8 +1,8 @@
+#!/usr/bin/env python
 # Copyright FuseSoC contributors
 # Licensed under the 2-Clause BSD License, see LICENSE for details.
 # SPDX-License-Identifier: BSD-2-Clause
 
-#!/usr/bin/env python
 import argparse
 import os
 import subprocess
