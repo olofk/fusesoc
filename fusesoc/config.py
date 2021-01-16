4615eb61645681cb878b166fa0d7e3280103dd6d
Philipp Wagner <mail@philipp-wagner.com>
2021-01-16 12:03:56 +0000

    Remove unused imports

--- fusesoc/config.py
+++ fusesoc/config.py
@@ -3,11 +3,9 @@
 # SPDX-License-Identifier: BSD-2-Clause
 
 import configparser
-import importlib
 import logging
 import os
 import sys
-from collections import OrderedDict
 from configparser import ConfigParser as CP
 
 from fusesoc.librarymanager import Library
