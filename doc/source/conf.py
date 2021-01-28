93c95c0544d4163181cb3add8af2e90f7e9f4510
Philipp Wagner <mail@philipp-wagner.com>
2021-01-28 16:18:56 +0000

    Show actual version output for documented version
    When building the documentation we now execute `fusesoc --version` to
    ensure that the documentation gives exactly the same output as the user
    is expected to get, as long as they are reading the documentation
    matching their installed version.
    
    As a slight downside, we loose syntax highlighting for that block,
    making it inconsistent with the other command snippets on the page. It's
    fixable, but didn't seem worth it for now.

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -55,6 +55,7 @@ extensions = [
     "sphinx.ext.githubpages",
     "sphinx.ext.intersphinx",
     "sphinx.ext.todo",
+    "sphinxcontrib.programoutput",
 ]
 
 intersphinx_mapping = {"https://docs.python.org/3": None}
