247dbd0dc23a669ef5b8d0ef81322dc3f4aee527
Erik Waling <erik.waling@gmail.com>
2023-04-17 10:36:07 +0200

    Add cores_root, resolve_env_vars_early, allow_additional_properties, verbose, no_export and system_name properties to Config class and make it possible to override config options from args

--- fusesoc/config.py
+++ fusesoc/config.py
@@ -145,9 +145,15 @@ class Config:
     def _set_default_section(self, name, val):
         self._cp.set(Config.default_section, name, str(val))
 
+    def _arg_or_val(self, arg, val):
+        if hasattr(self, arg):
+            return getattr(self, arg)
+        else:
+            return val
+
     @property
     def build_root(self):
-        return self._get_build_root()
+        return self._arg_or_val("args_build_root", self._get_build_root())
 
     @build_root.setter
     def build_root(self, val):
@@ -170,6 +176,14 @@ class Config:
         self._set_default_section("library_root", val)
 
     @property
+    def cores_root(self):
+        return self._arg_or_val("args_cores_root", self._paths_from_cfg("cores_root"))
+
+    @cores_root.setter
+    def cores_root(self, val):
+        self._set_default_section("cores_root", val)
+
+    @property
     def ignored_dirs(self):
         return self._get_ignored_dirs()
 
@@ -178,6 +192,59 @@ class Config:
         self._set_default_section(
             "ignored_dirs", " ".join(val) if type(val) == list else val
         )
+
+    @property
+    def resolve_env_vars_early(self):
+        return self._arg_or_val(
+            "args_resolve_env_vars_early",
+            self._cp.getboolean(
+                Config.default_section, "resolve_env_vars_early", fallback=False
+            ),
+        )
+
+    @resolve_env_vars_early.setter
+    def resolve_env_vars_early(self, val):
+        self._set_default_section("resolve_env_vars_early", val)
+
+    @property
+    def allow_additional_properties(self):
+        return self._arg_or_val(
+            "args_allow_additional_properties",
+            self._cp.getboolean(
+                Config.default_section, "allow_additional_properties", fallback=False
+            ),
+        )
+
+    @allow_additional_properties.setter
+    def allow_additional_properties(self, val):
+        self._set_default_section("allow_additional_properties", val)
+
+    @property
+    def verbose(self):
+        # Runtime config only, not possible to set in config file
+        return self._arg_or_val("args_verbose", False)
+
+    @property
+    def no_export(self):
+        return self._arg_or_val(
+            "args_no_export",
+            self._cp.getboolean(Config.default_section, "no_export", fallback=False),
+        )
+
+    @no_export.setter
+    def no_export(self, val):
+        self._set_default_section("no_export", val)
+
+    @property
+    def system_name(self):
+        return self._arg_or_val(
+            "args_system_name",
+            self._cp.get(Config.default_section, "system_name", fallback=None),
+        )
+
+    @system_name.setter
+    def system_name(self, val):
+        self._set_default_section("system_name", val)
 
     def write(self):
         if not hasattr(self, "_path"):
