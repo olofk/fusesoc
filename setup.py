409857e3e77b915097675debee0b6540956bfc20
Erik Waling <erik.waling@gmail.com>
2023-04-26 15:45:43 +0200

    Add myst_parser to setup.py install_requires list

--- setup.py
+++ setup.py
@@ -57,6 +57,7 @@ setup(
         "simplesat>=0.8.0",
         "fastjsonschema",
         "jsonschema2md",
+        "myst_parser",
     ],
     # Supported Python versions: 3.6+
     python_requires=">=3.6, <4",
