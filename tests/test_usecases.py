338f757e95574c5cbec571b842623638671a42d5
Julian Frimmel <julian@fri-me.de>
2025-01-18 20:11:37 +0100

    Don't error out on git libraries at fixed tags
    See #721 for context. This simply uses `git fetch` instead of `git pull`
    if a specific version is requested. This is a kind of hacky solution,
    since this won't work if somebody removes the `sync-version` from the
    `fusesoc.conf`-file (in that case, the code will use `git pull` again,
    which will fail as before). Since this usecase is not that common, this
    commit should be enough for now.

--- tests/test_usecases.py
+++ tests/test_usecases.py
@@ -32,6 +32,30 @@ def test_git_library_with_default_branch_is_added_and_updated(caplog):
     assert "Updating..." in caplog.text
 
 
+def test_update_git_library_with_fixed_version(caplog, capsys):
+    """
+    Previously, one could not successfully use `fusesoc library update` on
+    libraries with specific sync versions. This test checks, that no error is
+    reported in that case.
+    """
+    url = "https://github.com/fusesoc/fusesoc-generators"
+    _fusesoc("library", "add", url, "--sync-version", "v0.1.4")
+    assert "Checked out fusesoc-generators at version v0.1.4" in caplog.text
+
+    _fusesoc("library", "list")
+    output = capsys.readouterr().out
+    assert "fusesoc-generators" in output
+    assert "v0.1.4" in output
+
+    _fusesoc("library", "update")
+    assert "Failed to update library" not in caplog.text
+
+    _fusesoc("library", "list")
+    output = capsys.readouterr().out
+    assert "fusesoc-generators" in output
+    assert "v0.1.4" in output
+
+
 def test_usage_error_leads_to_nonzero_exit_code():
     with pytest.raises(SystemExit):
         _fusesoc("--option_does_not_exist")
