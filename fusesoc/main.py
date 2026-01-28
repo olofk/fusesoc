c01411aea6534ff712d39b085aa85eda6d42eeb3
ArkoshEternal <duncanparke@proton.me>
2026-01-27 19:31:30 -0500

    add variables cli arg --var and passing to class functions

--- fusesoc/main.py
+++ fusesoc/main.py
@@ -327,6 +327,15 @@ def run(fs, args):
         else:
             flags[flag] = True
 
+    variables = {}
+    for variable in args.var:
+        if "=" in variable:
+            var_name, var_value = variable.split("=", 1)
+            variables[var_name] = var_value
+        else:
+            logger.error(f"Invalid variable format: {variable}. Use NAME=VALUE.")
+            exit(1)
+
     fs.cm.db.mapping_set(args.mapping)
 
     if args.lockfile is not None:
@@ -339,7 +348,16 @@ def run(fs, args):
     core = _get_core(fs, args.system)
 
     try:
-        flags = dict(core.get_flags(flags["target"]), **flags)
+        flags = dict(core.get_flags(flags["target"], variables), **flags)
+    except SyntaxError as e:
+        logger.error(str(e))
+        exit(1)
+    except RuntimeError as e:
+        logger.error(str(e))
+        exit(1)
+
+    try:
+        variables = dict(core.get_variables(flags["target"]), **variables)
     except SyntaxError as e:
         logger.error(str(e))
         exit(1)
@@ -349,9 +367,9 @@ def run(fs, args):
 
     # Unconditionally clean out the work root on fresh builds
     # if we use the old tool API or clean flag is set
-    if do_configure and (not core.get_flow(flags) or args.clean):
+    if do_configure and (not core.get_flow(flags, variables) or args.clean):
         try:
-            prepare_work_root(fs.get_work_root(core, flags))
+            prepare_work_root(fs.get_work_root(core, flags, variables))
         except RuntimeError as e:
             logger.error(e)
             exit(1)
@@ -359,7 +377,7 @@ def run(fs, args):
     # Frontend/backend separation
 
     try:
-        edam_file, backend = fs.get_backend(core, flags, args.backendargs)
+        edam_file, backend = fs.get_backend(core, flags, variables, args.backendargs)
 
     except RuntimeError as e:
         logger.error(str(e))
@@ -684,6 +702,12 @@ def get_parser():
     parser_run.add_argument(
         "--flag",
         help="Set custom use flags. Can be specified multiple times",
+        action="append",
+        default=[],
+    )
+    parser_run.add_argument(
+        "--var",
+        help="Set custom variables. Can be specified multiple times",
         action="append",
         default=[],
     )
