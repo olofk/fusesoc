39e60d2d8d9b35eb7c9d389237ff6845bf11464e
Erik Waling <erik.waling@gmail.com>
2026-06-16 15:09:01 +0200

    Fix Python 3 subprocess compatibility by wrapping map() in list()

--- fusesoc/utils.py
+++ fusesoc/utils.py
@@ -36,7 +36,7 @@ class Launcher:
         logger.debug("    " + str(self))
         try:
             subprocess.check_call(
-                map(str, [self.cmd] + self.args),
+                list(map(str, [self.cmd] + self.args)),
                 cwd=self.cwd,
             ),
         except FileNotFoundError:
