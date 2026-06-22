a604450204a09bb8d813dcd0aa25c5b94d8a0ed2
Erik Waling <erik.waling@gmail.com>
2026-06-22 08:16:13 +0200

    Add ty equivalents of mypy type ignore directives

--- fusesoc/edalizer.py
+++ fusesoc/edalizer.py
@@ -765,5 +765,5 @@ class Ttptttg:
                     except SyntaxError as e:
                         w = "Failed to parse generated core file " + f + ": " + e.msg
                         raise RuntimeError(w)
-        logger.debug("Found " + ", ".join(str(c.name) for c in cores))  # type: ignore[attr-defined]
+        logger.debug("Found " + ", ".join(str(c.name) for c in cores))  # type: ignore[attr-defined, ty:unresolved-attribute]
         return cores
